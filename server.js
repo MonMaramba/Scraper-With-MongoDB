2d1300986a0143ff5a5351a68a673d15dab07153
unknown <monmaramba@yahoo.com>
2018-10-08 15:45:27 -0700

    mongo_uri correction

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.set("view engine", "handlebars");
 var PORT = process.env.PORT || 3000;
 
 // If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines"
+var MONGODB_URI = process.env.MONGO_URI || "mongodb://localhost/mongoHeadlines"
 mongoose.Promise = Promise;
 mongoose.connect(MONGODB_URI);
 
